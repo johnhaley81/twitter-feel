bdaad49660bca353a6523e7fd1e73db707bdd2b4
Kyle Smith <kyle.r.smiff@gmail.com>
2017-05-11 15:37:00 -0700

    Display significant watson tone results with tweets

--- src/routes/Search/components/Search.jsx
+++ src/routes/Search/components/Search.jsx
@@ -1,12 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import R from 'ramda';
+import R, { __ } from 'ramda';
 import TweetEmbed from 'react-tweet-embed';
+import emoji from 'node-emoji';
 
-const getEmbeddedTweets = R.map(
-  R.pipe(
-    R.view(R.lensPath(['tweet', 'id_str'])),
-    id => <TweetEmbed id={id} key={id} />
+const emojisByTone = {
+  anger: emoji.get('angry'),
+  disgust: emoji.get('mask'),
+  fear: emoji.get('fearful'),
+  joy: emoji.get('smiley'),
+  sadness: emoji.get('disappointed'),
+  analytical: emoji.get('sleuth_or_spy'),
+  confident: emoji.get('relieved'),
+  tentative: emoji.get('grimacing'),
+  openness_big5: emoji.get('speaking_head_in_silhouette'),
+  conscientiousness_big5: emoji.get('angel'),
+  extraversion_big5: emoji.get('dancer'),
+  agreeableness_big5: emoji.get('ok_hand'),
+  emotional_range_big5: emoji.get('interrobang')
+};
+
+const getEmbeddedTweet = R.pipe(
+  R.view(R.lensPath(['tweet', 'id_str'])),
+  id => <TweetEmbed id={id} key={id} />
+);
+
+const isToneSignificant = R.pipe(
+  R.view(R.lensPath(['score'])),
+  R.gt(__, 0.75)
+);
+
+const getEmojiFromTone = tone => (
+  <span style={{ fontSize: 30 }} title={`${tone.tone_name}: ${tone.score}`}>{emojisByTone[tone.tone_id]}</span>
+);
+
+const getTones = R.pipe(
+  R.view(R.lensPath(['tone', 'document_tone', 'tone_categories'])),
+  R.map(R.view(R.lensPath(['tones']))),
+  R.flatten,
+  R.filter(isToneSignificant),
+  R.map(getEmojiFromTone)
+);
+
+const getResults = R.map(
+  result => (
+    <div style={{ width: '100%', marginBottom: 10 }}>
+      <div style={{ display: 'inline-block', border: 'solid 1px black', padding: 20 }}>
+        <div>{getTones(result)}</div>
+        <div style={{ display: 'inline-block' }}>{getEmbeddedTweet(result)}</div>
+      </div>
+    </div>
   )
 );
 
@@ -18,7 +61,7 @@ export const Search = ({ fetchSearchResults, query, results, updateSearchQuery }
       Submit
     </button>
     <ul>
-      {getEmbeddedTweets(results)}
+      {getResults(results)}
     </ul>
   </div>
 );
